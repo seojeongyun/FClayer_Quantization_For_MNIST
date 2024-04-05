2e8b2d7e9398efb9733d7ee3e75094d882a782cd
SeoJeongYun <tjwjddbs4018@naver.com>
2024-04-05 21:24:41 +0900

    .

---
+++ dataset/get_mnist.py
@@ -0,0 +1,47 @@
+
+import os
+import struct
+import numpy as np
+
+
+def read(dataset = "training", path = "."):
+
+    if dataset is "training":
+        fname_img = os.path.join(path, 'train-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels.idx1-ubyte')
+    elif dataset is "testing":
+        fname_img = os.path.join(path, 't10k-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels.idx1-ubyte')
+    else:
+        raise ValueError("dataset must be 'testing' or 'training'")
+
+    # Load everything in some numpy arrays
+    with open(fname_lbl, 'rb') as flbl:
+        magic, num = struct.unpack(">II", flbl.read(8))
+        lbl = np.fromfile(flbl, dtype=np.int8)
+
+    with open(fname_img, 'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+        img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
+
+    get_img = lambda idx: (lbl[idx], img[idx])
+
+    mnist_data = []
+    for i in range(len(lbl)):
+        mnist_data.append(get_img(i))
+    return mnist_data
+
+
+def show(image):
+    from matplotlib import pyplot
+    import matplotlib as mpl
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
+    imgplot.set_interpolation('nearest')
+    ax.xaxis.set_ticks_position('top')
+    ax.yaxis.set_ticks_position('left')
+    pyplot.show()
+
+if __name__ == '__main__':
+    a = read(dataset='training', path='/home/jysuh/Downloads')
---
+++ dataset/mnist.py
@@ -0,0 +1,136 @@
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import struct
+import numpy as np
+
+from torch.utils.data import Dataset
+import numpy as np
+from scipy import io
+
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import glob
+
+
+class data_loader(Dataset):
+    def __init__(self, path, height=28, width=28, augmentation=False, task='train'):
+        super().__init__()
+        assert task == 'train' or task == 'val' or task == 'test', f'Invalid task...'
+        #
+        self.mnist = self.get_mnist(task, path)
+        #
+        self.dataset_path = path
+        self.task = task
+        #
+        self.img_size = (height, width)
+        self.augmentation = augmentation
+        #
+        self.fn_transform = self.get_transformer()
+
+    def get_mnist(self):
+        if self.task is "training":
+            fname_img = os.path.join(self.dataset_path, 'train-images.idx3-ubyte')
+            fname_lbl = os.path.join(self.dataset_path, 'train-labels.idx1-ubyte')
+        elif self.task is "testing":
+            fname_img = os.path.join(self.dataset_path, 't10k-images.idx3-ubyte')
+            fname_lbl = os.path.join(self.dataset_path, 't10k-labels.idx1-ubyte')
+        else:
+            raise ValueError("dataset must be 'testing' or 'training'")
+
+        # Load everything in some numpy arrays
+        with open(fname_lbl, 'rb') as flbl:
+            magic, num = struct.unpack(">II", flbl.read(8))
+            lbl = np.fromfile(flbl, dtype=np.int8)
+
+        with open(fname_img, 'rb') as fimg:
+            magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+            img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
+
+        get_img = lambda idx: (lbl[idx], img[idx])
+
+        mnist_data = []
+        for i in range(len(lbl)):
+            mnist_data.append(get_img(i))
+
+        return mnist_data
+    def __len__(self):
+        return len(self.mnist)
+
+    def __getitem__(self, index):
+        img = torch.tensor(self.mnist[index][1])
+        label = self.mnist[index][0]
+
+
+        return img, label
+
+    @staticmethod
+    def mat_to_rgb(image):
+        image_rgb = np.zeros([image.shape[0], image.shape[1], 3])
+        image_rgb[np.where(image == 0)] = (0, 0, 0)
+        image_rgb[np.where(image == 1)] = (71, 100, 100)
+        image_rgb[np.where(image == 2)] = (255, 228, 0)
+        return image_rgb
+
+    def get_transformer(self):
+        if self.augmentation:
+            if self.task == 'train':
+                fn_trans = transforms.Compose(
+                    [
+                        transforms.Resize(self.img_size,
+                                          interpolation=transforms.InterpolationMode.NEAREST),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.RandomVerticalFlip(),
+                        transforms.RandomRotation(degrees=(0, 90)),
+                        transforms.ToTensor(),
+                    ]
+                )
+            else:
+                fn_trans = transforms.Compose(
+                    [
+                        transforms.Resize(self.img_size,
+                                          interpolation=transforms.InterpolationMode.NEAREST),
+                        transforms.ToTensor()
+                    ]
+                )
+        else:
+            fn_trans = transforms.Compose(
+                [
+                    transforms.Resize(self.img_size,
+                                      interpolation=transforms.InterpolationMode.NEAREST),
+                    transforms.ToTensor()
+                ]
+            )
+        return fn_trans
+
+    @staticmethod
+    def collate_fn(batch):
+        """Merges a list of samples to form a mini-batch of Tensor(s)"""
+        inputs, labels = zip(*batch)
+        #
+        inputs = torch.cat(inputs, dim=0)
+        labels = torch.cat(labels, dim=0)
+        #
+        return inputs, labels
+
+
+if __name__ == '__main__':
+    object = data_loader(path='/storage/hrlee/WDM/wdmmix_new/sample_train/',
+                 height=52,
+                 width=52,
+                 augmentation=True,
+                 task='train'
+                 )
+    img, label = object.__getitem__(0)
+    #
+    loader = torch.utils.data.DataLoader(
+        object,
+        batch_size=5,
+        collate_fn=object.collate_fn
+    )
+    #
+    for batch_id, data in enumerate(loader):
+        image, label = data[0], data[1]
